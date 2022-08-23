ba97811fc4f0d971dc28e82130bd2586ab81f80f
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-08-22 19:34:20 -0600

    Initial Commit

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "time-tracker-ts" is now active!');
+}
+
+export function deactivate() { }
