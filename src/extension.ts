54a21376836ad99538ab3d0f21282bfc5341fbbd
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-08-24 00:40:32 -0600

    getting ready for better server tools

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,7 @@
 import * as vscode from 'vscode';
-import { startServer } from './tools/serverTools';
-import env from 'dotenv';
-env.config();
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "time-tracker-ts" is now active!');
-
-	startServer();
 }
 
 export function deactivate() { }
