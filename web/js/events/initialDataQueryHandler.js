7d65092e11b9c04f714ca9b065ccb5348d858105
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-09-10 19:01:46 -0600

    added vite

--- web/js/events/initialDataQueryHandler.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("initialDataQuery", (e) => {
-    let statuesImg = document.querySelector("#status-img");
-    statuesImg.title = "Fetched Data."
-    statuesImg.src = "./svgs/check.svg";
-
-    loadWindow(1);
-
-    globalMainChart = genNewChart(e.detail.query.data, null);
-});
--- web/js/tools/random.js
+++
@@ -1,6 +0,0 @@
-function setBigHiddenText(text) {
-    let a = document.getElementById("main-time-chart-container");
-    let b = a.querySelector("span");
-
-    b.innerText = text;
-}
