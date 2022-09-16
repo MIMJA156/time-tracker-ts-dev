81fc2c4c7674235701a251ca9f5f17fe76257e8c
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-09-16 01:33:09 -0600

    Getting Ready to add the Calender

--- src/web/src/main.ts
+++ src/web/src/main.ts
@@ -1,9 +1,13 @@
 import "./scss/main.scss";
+import "@fortawesome/fontawesome-free";
+
 import { closeWindow, loadWindow } from "./tools/windowLoader";
 
 import "./ts/windows/options";
 import "./ts/windows/movement";
 import "./ts/windows/settings";
+
+import "./ts/calender/scripts";
 
 // @ts-ignore
 window.loadWindow = loadWindow;
