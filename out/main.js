b11a8883d0cfd512dc9b64e6e8cccc6198ea79c6
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-10-11 23:04:14 -0600

    configured compilers / tasks | added svgs

---
+++ out/main.js
@@ -0,0 +1,44 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/extension/extension.ts
+var extension_exports = {};
+__export(extension_exports, {
+  activate: () => activate,
+  deactivate: () => deactivate
+});
+module.exports = __toCommonJS(extension_exports);
+
+// src/extension/func.ts
+function veryNice() {
+  return true;
+}
+
+// src/extension/extension.ts
+function activate(context) {
+  console.log("Hello World from Time Tracker!");
+  console.log(veryNice());
+}
+function deactivate() {
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  activate,
+  deactivate
+});
---
+++ src/extension/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Hello World from Time Tracker!');
+}
+
+export function deactivate() { }
