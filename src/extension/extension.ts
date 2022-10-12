291ac03813ca581342444c8b08b32bfbf1ea53d8
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-10-12 00:38:33 -0600

    got basic git oauth done

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,6 +1,16 @@
+
 import * as vscode from "vscode";
+import { getGist } from "./tools/gistTools";
+import { bootServer, killServer } from "./tools/serverTools";
+import { getLocalStoredSettings, runModelMatching, setStoragePaths } from "./tools/storageTools";
 
 export function activate(context: vscode.ExtensionContext) {
+    setStoragePaths(context);
+    runModelMatching();
+
+    let currentLocalSettings = getLocalStoredSettings();
+    getGist(currentLocalSettings.git.access_token, currentLocalSettings.git.gist_id);
+
     console.log('Hello World from Time Tracker!');
 }
 
