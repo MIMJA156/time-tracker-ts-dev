820346387eb89c2091dea672890b9c9f3e6a48b3
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-11-07 22:00:57 -0700

    added more window tools

--- src/web/src/ts/expose.ts
+++ src/web/src/ts/expose.ts
@@ -11,6 +11,49 @@ export default () => {
 			console.log('Using parent class: ', parent);
 			console.log('Using index: ', index);
 		}
+
+		hide(id: string) {
+			$('#' + id).addClass('hide');
+		}
+
+		show(id: string) {
+			$('#' + id).removeClass('hide');
+		}
+
+		toggle(id: string) {
+			if ($('#' + id).hasClass('hide')) {
+				$('#' + id).removeClass('hide');
+			} else {
+				$('#' + id).addClass('hide');
+			}
+		}
+
+		// hide() {
+		// 	let eventTarget = $(event?.target as HTMLElement);
+
+		// 	let window: JQuery<HTMLElement> = $();
+		// 	let running = true;
+		// 	let depth = 0;
+
+		// 	while (running) {
+		// 		depth++;
+		// 		let possibleWindow = eventTarget;
+
+		// 		for (let i = 0; i < depth; i++) {
+		// 			let parent = possibleWindow.parent();
+
+		// 			if (parent.hasClass('moveable-window')) {
+		// 				window = parent;
+		// 				running = false;
+		// 				break;
+		// 			} else {
+		// 				possibleWindow = parent;
+		// 			}
+		// 		}
+		// 	}
+
+		// 	window.addClass('hide');
+		// }
 	}
 
 	class calendarTools {
