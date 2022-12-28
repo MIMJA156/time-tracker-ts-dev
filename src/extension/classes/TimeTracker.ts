e50265f5f3f37d213f6b288cf6ee36da9bbc9b3d
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-12-27 23:08:55 -0700

    completed bare-bones time tracking

--- src/extension/classes/TimeTracker.ts
+++ src/extension/classes/TimeTracker.ts
@@ -1,13 +1,64 @@
+import { StorageUtils } from './StorageUtils';
+
 export class TimeTracker {
-	constructor() {}
+	private storageUtilities: StorageUtils;
+	private sampleRate: number;
+	private timeObject: object;
+	private sessionStart: number = new Date().getTime();
 
-	public start() {}
+	private loop: string | number | NodeJS.Timer | undefined;
 
-	public stop() {}
+	constructor({ sampleRate, storageUtils }: { sampleRate: number; storageUtils: StorageUtils }) {
+		this.storageUtilities = storageUtils;
+		this.sampleRate = sampleRate;
 
-	private loadTimeObject() {}
+		this.timeObject = this.sanitizeTimeObject(this.storageUtilities.getLocalStoredTime());
+	}
 
-	private saveTimeObject() {}
+	public start() {
+		this.loop = setInterval(() => {
+			let current = new Date();
+			this.timeObject['time'][current.getFullYear()][current.getMonth() + 1][current.getDate()] = this.calculateTimeSpent(this.sessionStart, this.timeObject);
+		}, this.sampleRate);
+	}
 
-	private verifyTimeObject() {}
+	public stop() {
+		clearInterval(this.loop);
+	}
+
+	public save() {
+		let current = new Date();
+		this.timeObject['time'][current.getFullYear()][current.getMonth() + 1][current.getDate()] = this.calculateTimeSpent(this.sessionStart, this.timeObject);
+		this.storageUtilities.setLocalStoredTime(this.timeObject);
+	}
+
+	private sanitizeTimeObject(givenTimeObject: object) {
+		let currentDate = new Date();
+
+		if (!givenTimeObject['time']) {
+			givenTimeObject['time'] = {};
+		}
+
+		if (!givenTimeObject['time'][currentDate.getFullYear()]) {
+			givenTimeObject['time'][currentDate.getFullYear()] = {};
+		}
+
+		if (!givenTimeObject['time'][currentDate.getFullYear()][currentDate.getMonth() + 1]) {
+			givenTimeObject['time'][currentDate.getFullYear()][currentDate.getMonth() + 1] = {};
+		}
+
+		if (!givenTimeObject['time'][currentDate.getFullYear()][currentDate.getMonth() + 1][currentDate.getDate()]) {
+			givenTimeObject['time'][currentDate.getFullYear()][currentDate.getMonth() + 1][currentDate.getDate()] = {
+				total: 0,
+			};
+		}
+
+		return givenTimeObject;
+	}
+
+	private calculateTimeSpent(sessionStart: number, timeObject: object) {
+		let current = new Date();
+		let result = current.getTime() - sessionStart + timeObject['time'][current.getFullYear()][current.getMonth() + 1][current.getDate()];
+		return result;
+	}
 }
--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,7 +1,22 @@
 import * as vscode from 'vscode';
+import { StorageUtils } from './classes/StorageUtils';
+import { TimeTracker } from './classes/TimeTracker';
+import { seconds } from './func/timeConverters';
+
+var timeTracker: TimeTracker;
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Hello World from Time Tracker!');
+
+	const storageUtils = new StorageUtils(context);
+
+	timeTracker = new TimeTracker({
+		sampleRate: seconds(5),
+		storageUtils,
+	});
 }
 
-export function deactivate() {}
+export function deactivate() {
+	timeTracker.save();
+	timeTracker.stop();
+}
